1eec1203924aeb400f7ea6f2ebc4504168374737
Yuji Sato <--replace-all>
2024-01-18 19:36:03 +0100

    server.ts config

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,8 @@
 // eslint-disable-next-line no-undef
 import app from './config/express';
 import mongoose from './config/mongoose';
-
 import vars from './config/vars';
+
 // import { generateRandomStringByLength } from './utils/functions';
 // import Maintenance from './models/Maintenance';
 // import { makeAllPublic } from './api/helpers/uploadFileHelper';
@@ -15,37 +15,6 @@ mongoose.connect();
 
 app.listen(port, async () => {
   console.log(`server started on port ${port} (${env})`);
-  // const maintenances = await Maintenance.find();
-  // const promises = maintenances.map(async (maintenance) => {
-  //   const authToken = new AuthToken({
-  //     linkId: maintenance.linkId,
-  //     nonce: maintenance.nonce,
-  //     docHolder: {
-  //       ref: 'maintenances',
-  //       instanceId: maintenance._id
-  //     },
-  //     space: maintenance.space
-  //   });
-  //   maintenance.linkId = undefined;
-  //   maintenance.nonce = undefined;
-  //   await authToken.save();
-  //   await maintenance.save();
-  //   console.log(authToken);
-  // });
-  // const checks = await Check.find().populate('maintenance');
-  // await Promise.all(
-  //   checks.map(async (check) => {
-  //     check.entity = 'maintenances';
-  //     // check.total = check.total || 0;
-  //     await check.save();
-  //   })
-  // );
-  // const documents = await Mongoose.model('maintenances').find();
-  // for (const document of documents) {
-  //   document.space = document.mainSpace;
-  //   document.mainSpace = undefined;
-  //   await document.save();
-  // }
 });
 
 /**
