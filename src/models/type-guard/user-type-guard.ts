647fc0786a4de1194f9d34474768990d1e728acc
Yuji Sato <--replace-all>
2024-02-02 07:55:49 +0100

    unused type guard [to be deleted]

---
+++ src/models/type-guard/user-type-guard.ts
@@ -0,0 +1,12 @@
+import { ObjectId } from 'mongodb';
+import { RoleFields } from '../../types/mongoose-types/model-types/role-interface';
+import { IUser } from '../../types/mongoose-types/model-types/user-interface';
+
+export function getPopulatedRoleField({ role, user }: { role: RoleFields; user: IUser }) {
+  // if (user.role instanceof ObjectId) {
+  //   throw new Error('Role not populated');
+  // }
+  // if(role === 'inhabitant') {
+  //   return user.role[role] as ;
+  // }
+}
