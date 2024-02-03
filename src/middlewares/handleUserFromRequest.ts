d574e2abbc4efad95a81876a8f12c0d38f705a95
Yuji Sato <--replace-all>
2024-02-03 08:18:34 +0100

    !!handleUserFromRequest middleware: no checking of user role.!!

--- src/middlewares/handleUserFromRequest.ts
+++ src/middlewares/handleUserFromRequest.ts
@@ -10,41 +10,40 @@ export const handleUserFromRequest = (req: RequestCustom, res: Response, next: N
 
 // if user is present frm jwt token then set it to req.user
 // if not just pass without setting req.user
-const setUserInRequest =
-  (req: RequestCustom, res: Response, next: NextFunction) => async (err: any, jwtReturnedData: ReqUser & boolean, info: any) => {
-    try {
-      if (jwtReturnedData === false) {
-        return next();
-      }
-      const error = err || info;
-
-      if (error) {
-        throw error;
-      }
-      if (!jwtReturnedData) {
-        throw new Error('user not found');
-      }
-      // if (user.role === 'admin' && !user.organizationId) {
-      //   throw new Error('organization not found');
-      // }
-      // if (user.role === 'user' && !user.spaceId) {
-      //   throw new Error('space not found');
-      // }
-
-      // if (user.spaceId) {
-      //   req.query.space = user.spaceId;
-      // }
-      // if (user.organizationId) {
-      //   req.query.organization = user.organizationId;
-      // }
-      req.user = jwtReturnedData;
+const setUserInRequest = (req: RequestCustom, res: Response, next: NextFunction) => async (err: any, reqUser: ReqUser & boolean, info: any) => {
+  try {
+    if (reqUser === false) {
       return next();
-    } catch (error) {
-      logger.error(error.message || error);
-      res.status(httpStatus.UNAUTHORIZED).json({
-        success: false,
-        message: error.message,
-        user: null
-      });
     }
-  };
+    const error = err || info;
+
+    if (error) {
+      throw error;
+    }
+    if (!reqUser) {
+      throw new Error('user not found');
+    }
+    // if (user.role === 'admin' && !user.organizationId) {
+    //   throw new Error('organization not found');
+    // }
+    // if (user.role === 'user' && !user.spaceId) {
+    //   throw new Error('space not found');
+    // }
+
+    // if (user.spaceId) {
+    //   req.query.space = user.spaceId;
+    // }
+    // if (user.organizationId) {
+    //   req.query.organization = user.organizationId;
+    // }
+    req.user = reqUser;
+    return next();
+  } catch (error) {
+    logger.error(error.message || error);
+    res.status(httpStatus.UNAUTHORIZED).json({
+      success: false,
+      message: error.message,
+      user: null
+    });
+  }
+};
